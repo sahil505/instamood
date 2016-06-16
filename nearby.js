c8426235bbc98f2fad258395eeada2b996f8bb8c
Sahil Khokhjar <sahilkhokhar505@gmail.com>
2016-06-16 12:49:24 +0200

    Completed my instamood assignment (No partner)

---
+++ nearby.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var token = window.location.hash;
+  if (!token) {
+    window.location.replace("./login.html");
+  }
+  token = token.replace("#", "?");
+  var mediaUrl = API_DOMAIN + RECENT_MEDIA_PATH + token;
+
+  $.ajax({
+    method: "GET",
+    url: mediaUrl,
+    dataType: "jsonp",
+    success: handleResponse,
+    error: function() {
+      alert("there has been an error...")
+    }
+  });
+
+});
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var API_DOMAIN = "https://api.instagram.com/v1";
 var RECENT_MEDIA_PATH = "/users/self/media/recent";
-// what do you think a variable in all caps means?
-
 $(document).ready(function() {
   var token = window.location.hash;
   if (!token) {
     window.location.replace("./login.html");
   }
-  token = token.replace("#", "?"); // Prepare for query parameter
+  token = token.replace("#", "?");
   var mediaUrl = API_DOMAIN + RECENT_MEDIA_PATH + token;
 
   $.ajax({
@@ -18,7 +16,7 @@ $(document).ready(function() {
     error: function() {
       alert("there has been an error...")
     }
-  })
+  });
 });
 
 function handleResponse(response) {
@@ -48,111 +46,50 @@ function handleResponse(response) {
     var day = date.getDay();
     dayList.push(day);
   }
+  var mode = function mode(arr) {
+   var numMapping = {};
+   var greatestFreq = 0;
+   var mode;
+   arr.forEach(function findMode(number) {
+     numMapping[number] = (numMapping[number] || 0) + 1;
 
-    // function analyzeSentiments(data) {
-    //   $.each(data, function(index, value) {
-    //     var phrase = value.caption.text;
-    //     var SENTIMENT_API_BASE_URL =
-    //     "https://twinword-sentiment-analysis.p.mashape.com/analyze/";
-    //     $.ajax({
-    //       method: "POST",
-    //       url: SENTIMENT_API_BASE_URL,
-    //       headers: {
-    //         "X-Mashape-Key": "PZhVoSU58ZmshQLuImiWrQy04U3Rp1DYjXDjsnkodgl0Yg6Pwp"
-    //       },
-    //       data: {text: phrase},
-    //       success: function(response) {
-    //         console.log(response);
-    //         addSentiment(response.type, response.score, index);
-    //       }
-    //     });
-    //   });
-    // }
+     if (greatestFreq < numMapping[number]) {
+       greatestFreq = numMapping[number];
+       mode = number;
+     }
+   });
+   return +mode;
+ }
+ var daymode = mode(dayList);
+ var percentage = (user_likes/response.data.length)*100;
+ var average_likes = total_likes/response.data.length;
+ var average_caption_length = total_caption_length/response.data.length;
+ var average_hashTags = total_hashTags/response.data.length;
 
-//     function addSentiment(type, score, picNum) {
-//   // Find the post the corresponds to this sentiment
-//   var picDiv = $("#images" + picNum);
-//   // Create a sentiment div
-//   var sentimentDiv = $("<div></div>");
-//   var sentimentI = $("<i></i>");
-//   sentimentI.addClass("fa");
-//   // Add the appropriate smiley using FontAwesome
-//   var faClass = "fa-meh-o";
-//   if (type === "positive") {
-//     sentimentDiv.addClass("positive");
-//     faClass = "fa-smile-o";
-//   } else if (type === "negative") {
-//     sentimentDiv.addClass("negative");
-//     faClass = "fa-frown-o";
-//   }
-//   sentimentI.addClass(faClass);
-
-//   sentimentDiv.append("Sentiment: ").append(sentimentI)
-//   .append(" (score: " + score.toFixed(2) + ")");
-//   picDiv.append(sentimentDiv);
-  
-//   updateTotalSentiment(score);
-// }
-
-// var allSentimentScores = []; // Aggregator for all sentiments so far.
-// function updateTotalSentiment(score) {
-//   allSentimentScores.push(score);
-//   console.log(allSentimentScores, score);
-//   // Calculate the average sentiment.
-//   var sum = 0;
-//   for (var i=0; i<allSentimentScores.length; i++) {
-//     sum += allSentimentScores[i];
-//   }
-//   var avg = sum / allSentimentScores.length;
-
-//   // Add nice text and colors.
-//   var text = "Neutral"
-//   var textClass = "";
-//   if (avg > 0) {
-//     text = "Positive!";
-//     textClass = "positive";
-//   } else if (avg < 0) {
-//     text = "Negative :(";
-//     textClass = "negative";
-//   }
-
-//   $("#mood").html(text + " (score: " + avg + ")");
-//   $("#mood").addClass(textClass);
-// }
-var mode = function mode(arr) {
- var numMapping = {};
- var greatestFreq = 0;
- var mode;
- arr.forEach(function findMode(number) {
-   numMapping[number] = (numMapping[number] || 0) + 1;
-
-   if (greatestFreq < numMapping[number]) {
-     greatestFreq = numMapping[number];
-     mode = number;
-   }
- });
- return +mode;
+ $("#stats1").html("Ego Score : " + percentage);
+ $("#stats2").html("Popularity : " + average_likes);
+ $("#stats3").html("Active Day : " + daySet[daymode]);
+ $("#stats4").html("Brevity : " + average_caption_length);
+ $("#stats5").html("Visibility : " + average_hashTags);
+ for (var i=0; i<response.data.length; i++) {
+   $("#images").append("<div id=image-"+ i +">" + '<img src=' + response.data[i].images.standard_resolution.url + "></div>");
+   $("#images").append("<div></div>",'<br>');
+   $("#image-"+i).append("<div>" + response.data[i].caption.text + "</div>");
+   $("#images").append("<div></div>",'<br>');
+   $("#images").append("<div></div>",'<br>');
+   $.ajax({
+      method: "GET",
+      url:"https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=" + response.data[i].caption.text,
+      headers:{"X-Mashape-Key": "PZhVoSU58ZmshQLuImiWrQy04U3Rp1DYjXDjsnkodgl0Yg6Pwp",
+      "Accept": "application/json",
+    },
+    success: function (result) {
+      console.log(result);
+      console.log(result.score);
+      $("#stats6").append("<div>" +"->  "+ result.score + "</div>");
+      }
+  });
+ }
 }
-var daymode = mode(dayList);
-var percentage = (user_likes/response.data.length)*100;
-var average_likes = total_likes/response.data.length;
-var average_caption_length = total_caption_length/response.data.length;
-var average_hashTags = total_hashTags/response.data.length;
-
-$("#stats1").html("Ego Score : " + percentage);
-$("#stats2").html("Popularity : " + average_likes);
-$("#stats3").html("Active Day : " + daySet[daymode]);
-$("#stats4").html("Brevity : " + average_caption_length);
-$("#stats5").html("Visibility : " + average_hashTags);
-for (var i=0; i<response.data.length; i++) {
- $("#images").append("<div></div>",'<img src=' + response.data[i].images.standard_resolution.url + '>');
- $("#images").append("<div></div>",'<br>');
- $("#images").append("<div></div>",response.data[i].caption.text);
- $("#images").append("<div></div>",'<br>');
- $("#images").append("<div></div>",'<br>');
-
-}
-}
-
 
 
